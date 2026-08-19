75b9ddc9c6653553e509593d50153611e14cb106
Thorsten <tk@programmierung.deutschehandarbeit.de>
2026-08-19 18:46:25 +0200

    Add iOS build and development scripts

--- ae/src/install.ts
+++ ae/src/install.ts
@@ -10,7 +10,9 @@ export default defineInstallScript(api => {
   api.extendPackageJson({
     scripts: {
       pull: "git pull --rebase --autostash && npm run setup",
-      setup: "npm install && npm install --prefix src-capacitor"
+      setup: "npm install && npm install --prefix src-capacitor",
+      "build-ios": "quasar build -m capacitor -T ios --ide",
+      "dev-ios": "quasar dev -m capacitor -T ios"
     }
   });
 });
